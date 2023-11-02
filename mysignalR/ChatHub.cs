a2c4021456ea492f8b3b22e4396eb4fffe2e34db
zzplng <1050425503@qq.com>
2023-11-02 14:00:03 +0800

    添加项目文件。

---
+++ mysignalR/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace mysignalR
+{
+	public class ChatHub : Hub
+	{
+		public async Task SendMessage(string user, string message)
+		{
+			// 调用所有客户端的接收消息的方法
+			await Clients.All.SendAsync("ReceiveMessage", user, message);
+		}
+
+    }
+}
